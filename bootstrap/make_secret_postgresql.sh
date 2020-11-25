c56678c03a5936d34ed424489435da4a632ce57a
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2020-11-25 15:59:55 +0000

    Minor adjustments to Postgres

--- bootstrap/make_secret_postgresql.sh
+++ bootstrap/make_secret_postgresql.sh
@@ -9,7 +9,6 @@ kubectl create secret generic postgresql \
   -n persistence \
   --from-literal=postgresql-password="$(pwgen -s 20 1)" \
   --from-literal=postgresql-postgres-password="$(pwgen -s 20 1)" \
-  --from-literal=postgresql-replication-password="$(pwgen -s 20 1)" \
   --from-literal=repmgr-password="$(pwgen -s 20 1)" > postgresql-secret.yaml
 
 echo "Created new secret:"
