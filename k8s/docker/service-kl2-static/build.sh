abb796296e77c9ca36e5ef94fa00b8969355bd72
Stuart Wallace <stuart.wallace@opencredo.com>
2020-12-17 20:35:52 +0000

    Add KL2 static container and chart

---
+++ k8s/docker/service-kl2-static/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -x
+set -e
+
+if [[ "$1" != "--nopush" ]]; then
+    ACCOUNT=$(aws sts get-caller-identity | jq '.Account' -r)
+    REGION=$AWS_DEFAULT_REGION
+    if [[ -z "$REGION" ]]; then
+        REGION=$AWS_REGION
+    fi
+    if [[ -z "$REGION" ]]; then
+        REGION=ap-northeast-2
+    fi
+
+    URL="${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com"
+else
+    URL="local"
+fi
+
+echo Update UI submodule
+git submodule deinit -f cms-frontend-web
+git submodule update --init -- cms-frontend-web
+
+echo Build docker image
+docker build -t $URL/vietnam-kl2-static:latest .
+
+if [[ "$1" != "--nopush" ]]; then
+    echo Docker login
+    aws ecr get-login-password | docker login --username AWS --password-stdin $URL
+
+    docker push $URL/vietnam-kl2-static:latest
+fi
+
