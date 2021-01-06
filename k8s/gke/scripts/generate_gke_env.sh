ba258873986a62116603281f8fbc34e53fce1316
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2021-01-06 16:43:19 +0000

    Configurable host, database and username for MySQL

--- k8s/gke/scripts/generate_gke_env.sh
+++ k8s/gke/scripts/generate_gke_env.sh
@@ -7,6 +7,7 @@ env_validate $ENV
 
 CONFIG=$(cat ../env/$ENV/config.json)
 MYSQL_NAME=$(echo $CONFIG | jq -r '.gcp .mysql .name')
+MYSQL_USER=$(echo $CONFIG | jq -r '.gcp .mysql .user')
 POSTGRESQL_NAME=$(echo $CONFIG | jq -r '.gcp .postgresql .name')
 REDIS_NAME=$(echo $CONFIG | jq -r '.gcp .redis .name')
 
@@ -15,7 +16,9 @@ POSTGRES_IP=$(kubectl get sqlinstance $POSTGRESQL_NAME -n config-connector -o js
 REDIS_IP=$(kubectl get redisinstance $REDIS_NAME -n config-connector -o jsonpath="{.status .host}")
 
 echo "{
-    \"mysql_host\": \"$MYSQL_IP\", 
+    \"mysql_host\": \"$MYSQL_IP\",
+    \"mysql_database\": \"$MYSQL_NAME\",
+    \"mysql_username\": \"$MYSQL_USER\",
     \"postgresql_host\": \"$POSTGRES_IP\", 
     \"redis_host\": \"$REDIS_IP\" 
 }"
