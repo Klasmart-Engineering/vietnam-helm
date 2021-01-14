6f08645d462a401d81a220da1598ce323419fad7
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2021-01-14 14:17:18 +0000

    Fixed parameters passed to user service in GCP

--- k8s/gke/scripts/generate_gke_env.sh
+++ k8s/gke/scripts/generate_gke_env.sh
@@ -10,17 +10,19 @@ PROJECT=$(cat ../env/$ENV/tfvars.json | jq -r '.terraform_project')
 MYSQL_NAME=$(echo $CONFIG | jq -r '.gcp .mysql .name')
 MYSQL_USER=$(echo $CONFIG | jq -r '.gcp .mysql .user')
 POSTGRESQL_NAME=$(echo $CONFIG | jq -r '.gcp .postgresql .name')
+POSTGRESQL_USER=$(echo $CONFIG | jq -r '.gcp .postgresql .user')
 REDIS_NAME=$(echo $CONFIG | jq -r '.gcp .redis .name')
 
 MYSQL_IP=$(kubectl get sqlinstance $MYSQL_NAME -n config-connector -o jsonpath="{.status .privateIpAddress}")
-POSTGRES_IP=$(kubectl get sqlinstance $POSTGRESQL_NAME -n config-connector -o jsonpath="{.status .privateIpAddress}")
+POSTGRESQL_IP=$(kubectl get sqlinstance $POSTGRESQL_NAME -n config-connector -o jsonpath="{.status .privateIpAddress}")
 REDIS_IP=$(kubectl get redisinstance $REDIS_NAME -n config-connector -o jsonpath="{.status .host}")
 
 echo "{
     \"mysql_host\": \"$MYSQL_IP\",
     \"mysql_database\": \"$MYSQL_NAME\",
     \"mysql_username\": \"$MYSQL_USER\",
-    \"postgresql_host\": \"$POSTGRES_IP\", 
-    \"redis_host\": \"$REDIS_IP\",
-    \"service_kl2_static_bucket\": \"$PROJECT-service-kl2-static-cdn\"
+    \"postgresql_host\": \"$POSTGRESQL_IP\", 
+    \"postgresql_database\": \"$POSTGRESQL_NAME\",
+    \"postgresql_username\": \"$POSTGRESQL_USER\", 
+    \"redis_host\": \"$REDIS_IP\"
 }"
