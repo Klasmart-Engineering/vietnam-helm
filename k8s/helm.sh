2d60cb2f461a703778304b9ed27aeb41b2f771b4
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2021-02-11 07:14:04 +0000

    Forgot to check that provider is GCP for checks in Helm.sh

--- k8s/helm.sh
+++ k8s/helm.sh
@@ -6,21 +6,28 @@ ENV=$1
 CMD=${2:-apply}
 env_validate "$ENV"
 
-# Check that we're logged into the correct GCP project
-CURRENT_GOOGLE_CLOUD_PROJECT=$(gcloud config get-value project)
-EXPECTED_GOOGLE_CLOUD_PROJECT=$(../scripts/python/env_var.py $ENV "terraform_project")
-if [ "$CURRENT_GOOGLE_CLOUD_PROJECT" != "$EXPECTED_GOOGLE_CLOUD_PROJECT" ]; then
-    echo "ERROR: Please login to GCP project: $CURRENT_GOOGLE_CLOUD_PROJECT"
-    exit 1
-fi
 
-# Check that we're logged into the correct GKE cluster
-REGION=$(../scripts/python/env_var.py $ENV "terraform_region")
-EXPECTED_CONTEXT="gke_${CURRENT_GOOGLE_CLOUD_PROJECT}_${REGION}_kidsloop"
-CURRENT_CONTEXT=$(kubectl config current-context)
-if [ "$EXPECTED_CONTEXT" != "$CURRENT_CONTEXT" ]; then
-    echo "ERROR: Please ensure you are logged into the kidsloop GKE cluster in $CURRENT_GOOGLE_CLOUD_PROJECT"
-    exit 1
+PROVIDER=$(../scripts/python/env_var.py $ENV "provider")
+
+if [ "$PROVIDER" = "gcp" ]; then
+
+    # Check that we're logged into the correct GCP project
+    CURRENT_GOOGLE_CLOUD_PROJECT=$(gcloud config get-value project)
+    EXPECTED_GOOGLE_CLOUD_PROJECT=$(../scripts/python/env_var.py $ENV "terraform_project")
+    if [ "$CURRENT_GOOGLE_CLOUD_PROJECT" != "$EXPECTED_GOOGLE_CLOUD_PROJECT" ]; then
+        echo "ERROR: Please login to GCP project: $CURRENT_GOOGLE_CLOUD_PROJECT"
+        exit 1
+    fi
+
+    # Check that we're logged into the correct GKE cluster
+    REGION=$(../scripts/python/env_var.py $ENV "terraform_region")
+    EXPECTED_CONTEXT="gke_${CURRENT_GOOGLE_CLOUD_PROJECT}_${REGION}_kidsloop"
+    CURRENT_CONTEXT=$(kubectl config current-context)
+    if [ "$EXPECTED_CONTEXT" != "$CURRENT_CONTEXT" ]; then
+        echo "ERROR: Please ensure you are logged into the kidsloop GKE cluster in $CURRENT_GOOGLE_CLOUD_PROJECT"
+        exit 1
+    fi
+
 fi
 
 
