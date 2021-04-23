e572e3be594d3fd7fa3df96fac19354c7223f2e1
hainguyen-calmid <hai.nguyen@calmid.com>
2021-04-23 13:48:27 +0700

    update ecr-token cronjob script

--- k8s/bootstrap/make_secret_ecr.sh
+++ k8s/bootstrap/make_secret_ecr.sh
@@ -3,21 +3,22 @@ set +xeuo pipefail
 
 # This script expects aws-cli-v2 to be installed.
 # It will not work with aws-cli v1.18
+NS_KIDSLOOP=$NAMESPACE
 
 export AWS_DEFAULT_REGION=ap-northeast-2
-export AWS_ACCESS_KEY_ID=$(kubectl get secret -n okc ecr-credentials-pull -o jsonpath='{.data.aws_access_key_id}' | base64 --decode)
-export AWS_SECRET_ACCESS_KEY=$(kubectl get secret -n okc ecr-credentials-pull -o jsonpath='{.data.secret_access_key}' | base64 --decode)
+export AWS_ACCESS_KEY_ID=$(kubectl get secret -n $NS_KIDSLOOP ecr-credentials-pull -o jsonpath='{.data.aws_access_key_id}' | base64 --decode)
+export AWS_SECRET_ACCESS_KEY=$(kubectl get secret -n $NS_KIDSLOOP ecr-credentials-pull -o jsonpath='{.data.secret_access_key}' | base64 --decode)
 
 kubectl create secret docker-registry ecr-registry \
     --dry-run=client \
     -o yaml \
-    -n okc \
+    -n $NS_KIDSLOOP \
     --docker-server="494634321140.dkr.ecr.ap-northeast-2.amazonaws.com" \
     --docker-username=AWS \
     --docker-password="$(aws ecr get-login-password)" \
     --docker-email="foo@bar.com" > ecr-secret.yaml
 
-kubectl delete secret --ignore-not-found=true -n okc ecr-registry
+kubectl delete secret --ignore-not-found=true -n $NS_KIDSLOOP ecr-registry
 kubectl apply -f ecr-secret.yaml 
 
 rm ecr-secret.yaml
