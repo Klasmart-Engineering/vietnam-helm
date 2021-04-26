7774df470720193d23fbc5d42673d4f3bf11e5d9
Hai Nguyen <hai.nguyen@calmid.com>
2021-04-26 11:01:09 +0000

    Merged in vietnam/vng-environments (pull request #36)
    Vietnam/vng environments
    
    * change name for vietnam-sre-dev environment; use helm variables to enable or disable some helm charts;
    
    * add vietnam-vng-dev environments; change name for vietnam-sre-dev environment; use helm variables to enable or disable some helm charts;
    
    * change to use chart condition instead of if statement
    
    * clean if statements codes; changed to use chart condition
    
    * upgrade nginx-ingress, add some custom configurations \& annotations
    
    * change values for databases of vng environment
    
    * use helm variables for ecr-token repositories endpoint
    
    * update ecr-token cronjob script
    
    * add ecr-token-infra chart
    
    * group helm_redis helm_postgresql and helm_mysql into helm_persistence
    
    Approved-by: Guy Richardson
    Approved-by: Kusuma Asdani

--- k8s/bootstrap/make_secret_ecr_vn.sh
+++ k8s/bootstrap/make_secret_ecr_vn.sh
@@ -3,21 +3,22 @@ set +xeuo pipefail
 
 # This script expects aws-cli-v2 to be installed.
 # It will not work with aws-cli v1.18
+NS_KIDSLOOP=$NAMESPACE
 
 export AWS_DEFAULT_REGION=ap-southeast-1
-export AWS_ACCESS_KEY_ID=$(kubectl get secret -n okc ecr-credentials-vn-pull -o jsonpath='{.data.aws_access_key_id}' | base64 --decode)
-export AWS_SECRET_ACCESS_KEY=$(kubectl get secret -n okc ecr-credentials-vn-pull -o jsonpath='{.data.secret_access_key}' | base64 --decode)
+export AWS_ACCESS_KEY_ID=$(kubectl get secret -n $NS_KIDSLOOP ecr-credentials-vn-pull -o jsonpath='{.data.aws_access_key_id}' | base64 --decode)
+export AWS_SECRET_ACCESS_KEY=$(kubectl get secret -n $NS_KIDSLOOP ecr-credentials-vn-pull -o jsonpath='{.data.secret_access_key}' | base64 --decode)
 
 kubectl create secret docker-registry ecr-registry-vn \
     --dry-run=client \
     -o yaml \
-    -n okc \
+    -n $NS_KIDSLOOP \
     --docker-server="242787759841.dkr.ecr.ap-southeast-1.amazonaws.com" \
     --docker-username=AWS \
     --docker-password="$(aws ecr get-login-password)" \
     --docker-email="foo@bar.com" > ecr-secret.yaml
 
-kubectl delete secret --ignore-not-found=true -n okc ecr-registry-vn
+kubectl delete secret --ignore-not-found=true -n $NS_KIDSLOOP ecr-registry-vn
 kubectl apply -f ecr-secret.yaml 
 
 rm ecr-secret.yaml
